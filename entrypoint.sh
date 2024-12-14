4ceddff2a7ad672cb1f8361b5d6b461892ba5bf0
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2024-12-14 15:00:02 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,12 @@ set_output() {
 
 # Install necessary packages
 echo "Installing necessary packages..."
-sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install -y gperf gcc-multilib gcc-10-multilib g++-multilib g++-10-multilib libc6-dev lib32ncurses5-dev x11proto-core-dev libx11-dev tree lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc bc ccache lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 lzop pngcrush schedtool squashfs-tools imagemagick libbz2-dev lzma ncftp qemu-user-static libstdc++-10-dev libtinfo5 libgflags-dev libncurses5 python3 curl unzip
+sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick \
+    lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev \
+    libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev \
+    python3 python2 bash tmux ccache curl unzip
 
 # Install OpenJDK 8
 echo "Installing OpenJDK 8..."
@@ -34,13 +36,51 @@ cd "$MANIFEST_DIR"
 git config --global user.name "$GITHUB_ACTOR"
 git config --global user.email "$GITHUB_ACTOR_ID+$GITHUB_ACTOR@users.noreply.github.com"
 
+# Determine MANIFEST_URL based on MANIFEST_BRANCH and python version
+ANDROID_VERSION=$(echo "$MANIFEST_BRANCH" | cut -d'-' -f2)
+        if (( $(echo "$ANDROID_VERSION < 10.0" | bc -l) )); then
+          MANIFEST_URL=https://github.com/mlm-games/manifest_pb.git
+          echo "Installing Python 2 for legacy branches..."
+          sudo apt-get install -y python2
+          # Use update-alternatives to set python to point to python2
+          sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
+          sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+          sudo update-alternatives --set python /usr/bin/python2
+        else
+          MANIFEST_URL=https://github.com/PitchBlackRecoveryProject/manifest_pb.git
+          echo "No need to install Python 2 for this branch."
+          # Ensure python points to python3
+          sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+          sudo update-alternatives --set python /usr/bin/python3
+        fi
+
+echo "MANIFEST_URL=${MANIFEST_URL}" >> $GITHUB_ENV
+
 # Initialize the repo
 echo "Initializing PBRP repo..."
-repo init --depth=1 -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b "$MANIFEST_BRANCH"
+if [ -n "$MANIFEST_BRANCH" ]; then
+    echo "Initializing repo with branch: $MANIFEST_BRANCH"
+    repo init --depth=1 -u "$MANIFEST_URL" -b "$MANIFEST_BRANCH"
+else
+    echo "Initializing repo without specifying a branch (default branch will be used)"
+    repo init --depth=1 -u "$MANIFEST_URL"
+fi
 
 # Sync the repo
 echo "Syncing PBRP repo..."
 repo sync -j$(nproc --all) --force-sync
+
+
+# Save the temp tree in the manifest dir.
+pushd "$MANIFEST_DIR"
+
+
+# If DEVICE_TREE is not provided, default to the current repository
+if [ -z "$DEVICE_TREE" ]; then
+    DEVICE_TREE="https://github.com/${GITHUB_REPOSITORY}"
+    echo "DEVICE_TREE not specified. Using current repository: ${DEVICE_TREE}"
+    echo "DEVICE_TREE=${DEVICE_TREE}" >> $GITHUB_ENV
+fi
 
 # Clone device tree into a temporary directory
 echo "Cloning device tree..."
@@ -52,16 +92,16 @@ else
     git clone "$DEVICE_TREE" tmp_device_tree
 fi
 
-# Check if DEVICE_NAME or DEVICE_PATH are default or not provided
-if [ -z "$DEVICE_NAME" ] || [ -z "$DEVICE_PATH" ]; then
+# Check if DEVICE_NAME or DEVICE_PATH or MAKEFILE_NAME are not provided
+if [ -z "$DEVICE_NAME" ] || [ -z "$DEVICE_PATH" ] || [ -z "$MAKEFILE_NAME" ]; then
     echo "Extracting variables from .mk files..."
     cd tmp_device_tree
 
     # Initialize variables
-    DEVICE_MAKEFILE=""
-    DEVICE_DIRECTORY=""
     DEVICE_NAME=""
     BRAND=""
+    DEVICE_PATH=""
+    MAKEFILE_NAME=""
 
     # Search for .mk files recursively in the device tree
     mk_files=$(find . -type f -name '*.mk')
@@ -69,18 +109,18 @@ if [ -z "$DEVICE_NAME" ] || [ -z "$DEVICE_PATH" ]; then
     # Loop through each .mk file found
     for file in $mk_files; do
         # Extract variables using sed
-        makefile=$(sed -n 's/^[[:space:]]*PRODUCT_NAME[[:space:]]*:=\s*\(.*\)/\1/p' "$file")
-        brand=$(sed -n 's/^[[:space:]]*PRODUCT_BRAND[[:space:]]*:=\s*\(.*\)/\1/p' "$file")
-        codename=$(sed -n 's/^[[:space:]]*PRODUCT_DEVICE[[:space:]]*:=\s*\(.*\)/\1/p' "$file")
-        if [[ -n "$makefile" && -n "$brand" && -n "$codename" ]]; then
-            DEVICE_MAKEFILE="$makefile"
-            BRAND="$brand"
-            DEVICE_NAME="$codename"
-            DEVICE_PATH="device/$brand/$codename"
-            echo "DEVICE_MAKEFILE=${DEVICE_MAKEFILE}" >> $GITHUB_ENV
+        product_name=$(sed -n 's/^[[:space:]]*PRODUCT_NAME[[:space:]]*:=\s*\(.*\)/\1/p' "$file")
+        product_device=$(sed -n 's/^[[:space:]]*PRODUCT_DEVICE[[:space:]]*:=\s*\(.*\)/\1/p' "$file")
+        product_manufacturer=$(sed -n 's/^[[:space:]]*PRODUCT_MANUFACTURER[[:space:]]*:=\s*\(.*\)/\1/p' "$file")
+        if [[ -n "$product_name" && -n "$product_device" && -n "$product_manufacturer" ]]; then
+            DEVICE_NAME="$product_device"
+            BRAND="$product_manufacturer"
+            DEVICE_PATH="device/$BRAND/$DEVICE_NAME"
+            MAKEFILE_NAME="${product_name}"
             echo "DEVICE_NAME=${DEVICE_NAME}" >> $GITHUB_ENV
             echo "BRAND=${BRAND}" >> $GITHUB_ENV
             echo "DEVICE_PATH=${DEVICE_PATH}" >> $GITHUB_ENV
+            echo "MAKEFILE_NAME=${MAKEFILE_NAME}" >> $GITHUB_ENV
             break
         fi
     done
@@ -92,7 +132,7 @@ if [ -z "$DEVICE_NAME" ] || [ -z "$DEVICE_PATH" ]; then
     fi
 
     # Navigate back to the MANIFEST_DIR
-    cd "$MANIFEST_DIR"
+    popd
 
     # Move the device tree into the correct directory
     echo "Moving device tree to $DEVICE_PATH"
@@ -100,55 +140,24 @@ if [ -z "$DEVICE_NAME" ] || [ -z "$DEVICE_PATH" ]; then
     mv tmp_device_tree/* "$DEVICE_PATH/"
     rm -rf tmp_device_tree
 else
-    echo "Using provided DEVICE_NAME and DEVICE_PATH"
+    echo "Using provided DEVICE_NAME, DEVICE_PATH, and MAKEFILE_NAME"
     # Move device tree to the specified DEVICE_PATH
     mkdir -p "$DEVICE_PATH"
     mv tmp_device_tree/* "$DEVICE_PATH/"
     rm -rf tmp_device_tree
 fi
 
-# Check for DEVICE_MAKEFILE if not set
-if [ -z "$DEVICE_MAKEFILE" ]; then
-    echo "Checking for recovery makefile..."
-    if [ -f "$DEVICE_PATH/twrp_${DEVICE_NAME}.mk" ]; then
-        DEVICE_MAKEFILE="twrp_${DEVICE_NAME}"
-    elif [ -f "$DEVICE_PATH/omni_${DEVICE_NAME}.mk" ]; then
-        DEVICE_MAKEFILE="omni_${DEVICE_NAME}"
-    elif [ -f "$DEVICE_PATH/pb_${DEVICE_NAME}.mk" ]; then
-        DEVICE_MAKEFILE="pb_${DEVICE_NAME}"
-    else
-        echo "::error::No recovery makefile found!"
-        exit 1
-    fi
-    echo "DEVICE_MAKEFILE=${DEVICE_MAKEFILE}" >> $GITHUB_ENV
-fi
-
 # Set OUTPUT_DIR
 OUTPUT_DIR="$MANIFEST_DIR/out/target/product/$DEVICE_NAME"
 echo "OUTPUT_DIR=${OUTPUT_DIR}" >> $GITHUB_ENV
 
-# Install additional dependencies for legacy branches
-if [[ "$MANIFEST_BRANCH" != "android-11.0" && "$MANIFEST_BRANCH" != "android-12.1" ]]; then
-    echo "Installing Python 2 for legacy branches..."
-    sudo apt-get install -y python2
-    sudo ln -sf /usr/bin/python2 /usr/bin/python
-else
-    echo "No need to install Python 2 for this branch."
-fi
-
-# Fix missing fonts
-echo "Fixing missing fonts..."
-mkdir -p external/noto-fonts/other
-cd external/noto-fonts/other
-wget https://github.com/cd-Crypton/custom-recovery-extras/raw/main/missing-font.zip
-unzip -o missing-font.zip
-cd "$MANIFEST_DIR"
-
 # Build PBRP
 echo "Building PBRP..."
+cd "$MANIFEST_DIR"
+set +e
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-
+set -e
 if [ "$BUILD_TARGET" != "pbrp" ]; then
     lunch "${DEVICE_MAKEFILE}-eng"
     make clean
@@ -159,13 +168,15 @@ else
     mka -j$(nproc) "$BUILD_TARGET"
 fi
 
+echo "Done building your ${BUILD_TARGET}.img"
+
 # Set build date
 echo "BUILD_DATE=$(TZ=UTC date +%Y%m%d)" >> $GITHUB_ENV
 
 # Check if the recovery image exists
 echo "Checking if the recovery image exists..."
 img_file=$(find "$OUTPUT_DIR" -name "*.img" -print -quit)
-zip_file=$(find "$OUTPUT_DIR" -name "PBRP*.zip" -print -quit)
+zip_file=$(find "$OUTPUT_DIR" -name "*PBRP*zip" -print -quit)
 
 if [ -f "$img_file" ]; then
     echo "CHECK_IMG_IS_OK=true" >> $GITHUB_ENV
@@ -173,6 +184,7 @@ if [ -f "$img_file" ]; then
     echo "MD5_IMG=$(md5sum "$img_file" | cut -d ' ' -f 1)" >> $GITHUB_ENV
 else
     echo "::warning::Recovery image not found."
+    exit
 fi
 
 if [ -f "$zip_file" ]; then
